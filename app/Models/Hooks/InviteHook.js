93f775bbb6ae829ad3b8f92d10e6482fad3fd041
facundo prieto <facundo.prieto321@gmail.com>
2020-05-18 20:31:15 -0300

    InviteHook InviteController

---
+++ app/Models/Hooks/InviteHook.js
@@ -0,0 +1,16 @@
+const InviteHook = (exports = module.exports = {});
+
+const User = use('App/Models/User');
+
+InviteHook.sendInvitationEmail = async (invite) => {
+  const { email } = invite;
+  const invited = await User.findBy('email', email);
+
+  if (invited) {
+    await invited.teams().attach(invite.team_id);
+  } else {
+    console.log('CRIAR CONTA');
+
+    // ENVIO DE EMAIL
+  }
+};
