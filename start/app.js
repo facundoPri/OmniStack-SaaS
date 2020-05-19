67dd9dc587c11baedd10cc317032be6c2da3978b
facundo prieto <facundo.prieto321@gmail.com>
2020-05-19 20:00:52 -0300

    Roles and permition tables migrations seed update

--- start/app.js
+++ start/app.js
@@ -20,6 +20,7 @@ const providers = [
   '@adonisjs/mail/providers/MailProvider',
   '@adonisjs/framework/providers/ViewProvider',
   '@adonisjs/validator/providers/ValidatorProvider',
+  'adonis-acl/providers/AclProvider',
 ];
 
 /*
@@ -34,6 +35,7 @@ const providers = [
 const aceProviders = [
   '@adonisjs/lucid/providers/MigrationsProvider',
   'adonis-kue/providers/CommandsProvider',
+  'adonis-acl/providers/CommandsProvider',
 ];
 
 /*
@@ -48,7 +50,10 @@ const aceProviders = [
 |   { Route: 'Adonis/Src/Route' }
 |
 */
-const aliases = {};
+const aliases = {
+  Role: 'Adonis/Acl/Role',
+  Permission: 'Adonis/Acl/Permission',
+};
 
 /*
 |--------------------------------------------------------------------------
