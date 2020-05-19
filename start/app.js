c3e475a33d3d67e71a262292334f2719367c2d84
facundo prieto <facundo.prieto321@gmail.com>
2020-05-19 00:19:50 -0300

    Validations created

--- start/app.js
+++ start/app.js
@@ -19,6 +19,7 @@ const providers = [
   '@adonisjs/redis/providers/RedisProvider',
   '@adonisjs/mail/providers/MailProvider',
   '@adonisjs/framework/providers/ViewProvider',
+  '@adonisjs/validator/providers/ValidatorProvider',
 ];
 
 /*
